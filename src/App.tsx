293ca9953a6fbf40825dbd8b008f1c1ba91819f9
LuboslavBurdenski <luboslav990226@gmail.com>
2021-04-23 14:48:01 +0300

    ''

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
 import styled from 'styled-components';
-
+import { US_ELECTION_ADDRESS } from './constants';
+import { getContract } from './helpers/ethers';
+import US_ELECTION from './abis/US_ELECTION.json';
 import Web3Modal from 'web3modal';
+
 // @ts-ignore
 import WalletConnectProvider from '@walletconnect/web3-provider';
 import Column from './components/Column';
@@ -9,6 +12,7 @@ import Wrapper from './components/Wrapper';
 import Header from './components/Header';
 import Loader from './components/Loader';
 import ConnectButton from './components/ConnectButton';
+
 
 import { Web3Provider } from '@ethersproject/providers';
 import { getChainData } from './helpers/utilities';
@@ -19,6 +23,7 @@ const SLayout = styled.div`
   min-height: 100vh;
   text-align: center;
 `;
+
 
 const SContent = styled(Wrapper)`
   width: 100%;
@@ -48,6 +53,13 @@ const SBalances = styled(SLanding)`
   }
 `;
 
+const WrapperInputs = styled.section`
+  border-radius: 7px;
+  padding: 10px;
+  display: flex;
+  flex-flow: column wrap;
+`;
+
 interface IAppState {
   fetching: boolean;
   address: string;
@@ -58,6 +70,8 @@ interface IAppState {
   result: any | null;
   electionContract: any | null;
   info: any | null;
+  currentLeader: number | string,
+  showLeader: boolean,
 }
 
 const INITIAL_STATE: IAppState = {
@@ -69,7 +83,9 @@ const INITIAL_STATE: IAppState = {
   pendingRequest: false,
   result: null,
   electionContract: null,
-  info: null
+  info: null,
+  currentLeader: '',
+  showLeader: false
 };
 
 class App extends React.Component<any, any> {
@@ -106,18 +122,22 @@ class App extends React.Component<any, any> {
 
     const address = this.provider.selectedAddress ? this.provider.selectedAddress : this.provider?.accounts[0];
 
+    const electionContract = getContract(US_ELECTION_ADDRESS, US_ELECTION.abi, library, address);
+
     await this.setState({
+      provider: this.provider,
       library,
       chainId: network.chainId,
       address,
-      connected: true
+      connected: true,
+      electionContract
     });
 
     await this.subscribeToProviderEvents(this.provider);
 
   };
 
-  public subscribeToProviderEvents = async (provider:any) => {
+  public subscribeToProviderEvents = async (provider: any) => {
     if (!provider.on) {
       return;
     }
@@ -129,7 +149,7 @@ class App extends React.Component<any, any> {
     await this.web3Modal.off('accountsChanged');
   };
 
-  public async unSubscribe(provider:any) {
+  public async unSubscribe(provider: any) {
     // Workaround for metamask widget > 9.0.3 (provider.off is undefined);
     window.location.reload(false);
     if (!provider.off) {
@@ -142,12 +162,13 @@ class App extends React.Component<any, any> {
   }
 
   public changedAccount = async (accounts: string[]) => {
-    if(!accounts.length) {
+    if (!accounts.length) {
       // Metamask Lock fire an empty accounts array 
       await this.resetApp();
     } else {
       await this.setState({ address: accounts[0] });
     }
+
   }
 
   public networkChanged = async (networkId: number) => {
@@ -156,7 +177,7 @@ class App extends React.Component<any, any> {
     const chainId = network.chainId;
     await this.setState({ chainId, library });
   }
-  
+
   public close = async () => {
     this.resetApp();
   }
@@ -182,7 +203,39 @@ class App extends React.Component<any, any> {
     await this.unSubscribe(this.provider);
 
     this.setState({ ...INITIAL_STATE });
+  };
 
+  public currentLeader = async () => {
+    const { electionContract } = this.state;
+
+    const currentLeader = await electionContract.currentLeader();
+    console.log(this.state.showLeader);
+
+    await this.setState({ showLeader: !this.state.showLeader })
+    await this.setState({ currentLeader });
+  };
+
+  public submitElectionResult = async () => {
+    const { electionContract } = this.state;
+
+    // const dataArr = [
+    //   'Idaho',
+    //   51,
+    //   50,
+    //   24
+    // ];
+
+    console.log(electionContract)
+
+    // await this.setState({ fetching: true });
+    // const transaction = await electionContract.submitStateResult(dataArr);
+
+    // await this.setState({ transactionHash: transaction.hash });
+
+    // const transactionReceipt = await transaction.wait();
+    // if (transactionReceipt.status !== 1) {
+    //   // React to failure
+    // }
   };
 
   public render = () => {
@@ -209,11 +262,30 @@ class App extends React.Component<any, any> {
                 </SContainer>
               </Column>
             ) : (
-                <SLanding center>
-                  {!this.state.connected && <ConnectButton onClick={this.onConnect} />}
-                </SLanding>
-              )}
+              <SLanding center>
+                {!this.state.connected && <ConnectButton onClick={this.onConnect} />}
+              </SLanding>
+            )}
           </SContent>
+        </Column>
+        <Column maxWidth={1000} spanHeight>
+          <button onClick={this.currentLeader}>check leader</button>
+          <button onClick={this.submitElectionResult}>print contract</button>
+
+          {this.state.showLeader && <section>{this.state.currentLeader}</section>}
+          <WrapperInputs>
+            <section >
+              <label htmlFor="name">name</label>
+              <input name="name" placeholder="Chicago" />
+              <label htmlFor="votesB">votes Biden</label>
+              <input name="votesB" placeholder="500" />
+              <label htmlFor="votesT">votes Trump</label>
+              <input name="votesT" placeholder="500" />
+              <label htmlFor="seats">state seats</label>
+              <input name="stateSeats" placeholder="20" />
+            </section>
+          </WrapperInputs>
+
         </Column>
       </SLayout>
     );
